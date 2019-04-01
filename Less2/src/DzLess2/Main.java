0e0729c5ddb798b1b3aaca7f7cbc4e8b2bd7c80e
oirikata <oiri.kata@gmail.com>
2019-04-01 21:06:48 +0300

    Задан целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия замены 0 на 1, 1 на 0

--- Less2/src/DzLess2/Main.java
+++ Less2/src/DzLess2/Main.java
@@ -1,8 +1,18 @@
 package DzLess2;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	
+     int [] arr1 = {1,0,0,1,1,0,1,1,0,0};
+        for (int i = 0; i <arr1.length-1 ; i++) {
+            if (arr1[i]==1) arr1[i]=0;
+                    else arr1[i]=1;
+        }
+        System.out.println(Arrays.toString(arr1));
+
+
     }
 }
+
