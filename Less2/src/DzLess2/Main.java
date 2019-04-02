d5bc7188c57831eab148e55b6458adc96dd7eac0
oirikata <oiri.kata@gmail.com>
2019-04-02 20:49:33 +0300

    Навела красоту: вынесла упражнения в методы, добавила подписи заданий

--- Less2/src/DzLess2/Main.java
+++ Less2/src/DzLess2/Main.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+
+
      /*задание 1: Задать целочисленный массив, состоящий из элементов 0 и 1
       С помощью цикла и условия заменить 0 на 1, 1 на 0 */
+        System.out.println("Задание 1:");
         int [] arr1 = {1,0,0,1,1,0,1,1,0,0};
         for (int i = 0; i <arr1.length-1 ; i++) {
             if (arr1[i]==1) arr1[i]=0;
@@ -16,6 +19,7 @@ public class Main {
 
      //задание 2
      // Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+        System.out.println("Задание 2:");
         int [] arr2 = new int[8];
         for (int i = 0; i <arr2.length ; i++) {
             arr2[i]=3*i;
@@ -24,46 +28,68 @@ public class Main {
 
      //задание 3
      //Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+        System.out.println("Задание 3:");
         int[] arr3={ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
-        for (int i = 0; i <arr3.length ; i++) {
-            if (arr3[i]<6) arr3[i]=arr3[i]*2;
-        }
         System.out.println(Arrays.toString(arr3));
+        dubleLowerNumbers(arr3,6);
+
     //задание 4
     /*Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
      и с помощью цикла(-ов) заполнить его диагональные элементы единицами */
+        System.out.println("Задание 4:");
         int[][] arr4 = new int[6][6];
-        for (int i = 0; i <arr4.length ; i++) {
-           arr4[i][i]=1;
-           arr4[i][arr4.length-i-1]=1;
-        }
-        for (int i = 0; i < arr4.length; i++) {
-        System.out.println(Arrays.toString(arr4[i])); }
+        makeSquareDiagonal(arr4);
 
      //задание 5
      //Задать одномерный массив и найти в нем минимальный и максимальный элементы
+        System.out.println("Задание 5:");
         int[] arr5={ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
-        int max=0;
-        int min=arr5[0];
-        for (int i = 0; i <arr5.length ; i++) {
-            if (arr5[i]>max) max=arr5[i];
-            else if (arr5[i]<min) min=arr5[i];
-        }
-        System.out.println("Минимальное значение: "+min+" ,Максимальное значение: " +max);
+        lookforMinMax(arr5);
 
      //задание 6
+        System.out.println("Задание 6:");
         int[] arr6= {6,2,3,1,};
         boolean b = checkBalance(arr6);
         System.out.println((b==true) ? "Массив сбалансирован" : "массив не сбалансирован");
 
     //задание 7
+        System.out.println("Задание 7:");
         System.out.println(Arrays.toString(arr3));
         changeMassPosition(arr3,3);
 
     }
 
+    public static void lookforMinMax(int arr[]) {
+        System.out.println(Arrays.toString(arr));
+        int max=0;
+        int min=arr[0];
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i]>max) max=arr[i];
+            else if (arr[i]<min) min=arr[i];
+        }
+        System.out.println("Минимальное значение: "+min+" ,Максимальное значение: " +max);
+
+    }
+
+    public static void makeSquareDiagonal (int arr[][]){
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i][i]=1;
+            arr[i][arr.length-i-1]=1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i])); }
+    }
+
+    public static void dubleLowerNumbers(int[] arr, int max) {
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i]<max) arr[i]=arr[i]*2;
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
 
     public static boolean checkBalance(int[] arr) {
+        System.out.println(Arrays.toString(arr));
         int summ1=0;
         int summ2=0;
         boolean result=false;
