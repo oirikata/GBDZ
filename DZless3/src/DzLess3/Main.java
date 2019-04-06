41d24f3bcf1a7c7bf5e71b5077410621df6f691c
oirikata <oiri.kata@gmail.com>
2019-04-06 16:33:45 +0300

    Добавлена игра в слова

--- DZless3/src/DzLess3/Main.java
+++ DZless3/src/DzLess3/Main.java
@@ -15,6 +15,10 @@ public class Main {
 
        playNumbersGame();
 
+       //Задание 2: игра в слова.
+        playWordsGame();
+
+
     }
 
     private static void playNumbersGame() {
@@ -42,6 +46,46 @@ public class Main {
             again = sc.nextInt();
         } while (again==1);
 
+    }
+    private static void playWordsGame(){
+        Random rnd = new Random();
+        Scanner sc = new Scanner(System.in);
+        boolean win=false;
+        int again=1;
+
+        String arr[]={"apple", "orange", "lemon", "banana", "apricot", "avocado",
+                "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom",
+                "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        do {
+        String word = arr[rnd.nextInt(arr.length)];
+        System.out.println("Мы загадали число от 0 до 9. Угадай число за 3 попытки");
+        System.out.println(word);
+        do {
+            String guess = sc.next();
+            if (guess.equals(word)==false)
+            {
+                System.out.println("Увы, мы загадали другое слово. Посмотри, может быть, ты угадал какие-то буквы?");
+                int leng= (word.length()<guess.length()?word.length():guess.length());
+                for (int i = 0; i < leng; i++) {
+                    if (guess.charAt(i)==word.charAt(i))
+                        System.out.print(word.charAt(i));
+                    else System.out.print("#");
+                }
+                for (int i = leng; i <15 ; i++) {
+                    System.out.print("#");
+                }
+                System.out.println("#");
+
+            }
+            else
+                win=true;
+
+        } while (win==false);
+        win=false;
+        System.out.println("Поздравляем, вы выиграли!");
+        System.out.println("Повторить игру еще раз? 1- да/0-нет");
+        again = sc.nextInt();
+    } while (again==1);
     }
 
 
