043884633e3de21853d63d92139919172ba6367b
oirikata <oiri.kata@gmail.com>
2019-04-09 12:59:09 +0300

    сделана проверка checkWin с любым размером поля и длиной выигрышной комбинации

--- DZLess4/src/DzLess4/Main.java
+++ DZLess4/src/DzLess4/Main.java
@@ -4,12 +4,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static int size_y=3;
-    static int size_x=3;
+    static int size_y=5;
+    static int size_x=5;
     static char[][] field=new char [size_y][size_x];
     static char PLAYER_DOT = 'X';
     static char AI_DOT = 'O';
     static char EMPTY_DOT = '.';
+    static int vinNum=4; //какое количество фишек подряд надо собрать для победы
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
 
@@ -104,8 +105,41 @@ public class Main {
         return true;
     }
 
-    public static boolean checkWin(){
-        
+    public static boolean checkWin(char sym) {
+        int summ1, summ2, summ3, summ4;
+        for (int i = 0; i < size_y; i++) {
+            for (int j = 0; j < size_x; j++) {
+                if (field[j][i]==sym) {
+                    //горизонталь
+                    summ1=0;
+                    for (int k = 0; k <vinNum&&j+k<size_x; k++) {
+                        if (field[j+k][i]==sym) summ1++;
+                    }
+                    //вертикаль
+                    summ2=0;
+                    for (int k = 0; k <vinNum&&i+k<size_y; k++) {
+                        if (field[j][i+k]==sym) summ2++;
+                    }
+                    //диагональ вправо
+                    summ3=0;
+                    for (int k = 0; k <vinNum&&i+k<size_y&&j+k<size_x; k++) {
+                       if(field[j+k][i+k]==sym) summ3++;
+                    }
+                    //диагональ влево
+                    summ4=0;
+                    for (int k = 0; k <vinNum&&j-k>0&&i+k<size_y; k++) {
+                        if(field[j-k][i+k]==sym) summ4++;
+                    }
+                    if (summ1==vinNum||summ2==vinNum||summ3==vinNum||summ4==vinNum)
+                        return true;
+                    break;
+
+                }
+                else continue;
+            }
+
+        } return false;
+        }
     }
 
-}
+
