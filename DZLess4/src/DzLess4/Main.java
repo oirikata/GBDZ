fdc895f8573984a0eca07e9eb2c943bda7c5ada8
oirikata <oiri.kata@gmail.com>
2019-04-09 11:18:55 +0300

    поле заполнено пустыми, добавлена разметка и нумерация

--- DZLess4/src/DzLess4/Main.java
+++ DZLess4/src/DzLess4/Main.java
@@ -1,8 +1,45 @@
 package DzLess4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    static int size_y=3;
+    static int size_x=3;
+    static char[][] field=new char [size_y][size_x];
+    static char PLAYER_DOT = 'X';
+    static char AI_DOT = 'O';
+    static char EMPTY_DOT = '.';
+
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        initField();
+        printField();
+
     }
+    public static void printField () {
+        System.out.print("| ");
+        for (int i = 0; i <size_x ; i++) {
+            System.out.print("|"+ (i+1));
+        }
+        System.out.println("|");
+        for (int i = 0; i <size_y ; i++) {
+            System.out.print("|"+(i+1));
+            for (int j = 0; j <size_x ; j++) {
+                System.out.print("|"+field[i][j]);
+            }
+            System.out.println("|");
+        }
+    }
+    public static void initField() {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                field[i][j] = EMPTY_DOT;
+            }
+        }
+    }
+
 }
